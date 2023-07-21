5bd3711f502a7cea6c301352b5c79cf9b3d05631
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-07-21 16:50:43 +0300

    making the login form controlled

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Login from './Login';
 import Register from './Register';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/login' Component={Login}/>
+          <Route exact path='/' Component={Login}/>
           <Route exact path='/register' Component={Register}/>
+          <Route exact path='/home' Component={Home}/>
           </Routes>
       </Router>
     </div>
