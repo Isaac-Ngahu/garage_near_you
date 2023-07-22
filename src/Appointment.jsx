5fc45734b69ce38c32c7e4ad7b19c83d32ea982d
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-07-22 16:13:54 +0300

    adding input fields for customer information

--- src/Appointment.jsx
+++ src/Appointment.jsx
@@ -5,6 +5,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 function Appointment() {
     const [formData,setFormData] = useState({
         issue:'',
+        vehicleMake:'',
+        additionalNotes:'',
         datePicker:'',
         timePicker:''
     })
@@ -24,12 +26,16 @@ function Appointment() {
         <form className='booking-form' onSubmit={handleFormSubmit}>
             <label htmlFor="phoneNumber">Please Enter your phone Number:</label>
             <input type='phone' />
-            <label htmlFor='customer-issue'>Enter the issue you would like us to sort:</label>
-            <textarea type='text' id='issue' value={formData.issue} onChange={handleFormChange}></textarea>
+            <label htmlFor='customer-issue'>Enter the service type you would like sorted:</label>
+            <textarea type='text' id='issue' required value={formData.issue} onChange={handleFormChange}></textarea>
+            <label htmlFor="vehicle-type">Enter the make of the vehicle:</label>
+            <input type='text' id='vehicleMake' required value={formData.vehicleMake} onChange={handleFormChange} />
+            <label htmlFor="notes">Enter any additional notes or special services:</label>
+            <input type='text' id='additionalNotes' value={formData.additionalNotes} onChange={handleFormChange} />
             <div className='specific-time'><label htmlFor="date">Please select a date:</label>
             <div className='date-available'>Appointments are only available after <span>this date</span> moving forward </div>
-            <input type="date" id="datePicker" name="selectedDate" value={formData.datePicker} onChange={handleFormChange}/>
-            <input type="time" id="timePicker" name="selectedTime" value={formData.timePicker} onChange={handleFormChange}/>
+            <input type="date" id="datePicker" name="selectedDate" required value={formData.datePicker} onChange={handleFormChange}/>
+            <input type="time" id="timePicker" name="selectedTime" required value={formData.timePicker} onChange={handleFormChange}/>
             </div>
             <button type='submit'>submit</button>
         </form>
