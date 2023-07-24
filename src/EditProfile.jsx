82ef429bc0fc31fbbffb700ca7cfb0529aa1a825
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-07-24 23:26:02 +0300

    getting started on the readme

--- src/EditProfile.jsx
+++ src/EditProfile.jsx
@@ -3,6 +3,7 @@ import './EditProfile.css'
 import { useState } from 'react'
 function EditProfile() {
     const [newValue,setNewvalue] = useState("")
+    const id = sessionStorage.getItem("user_id")
     function handleSubmit(e){
         e.preventDefault()
         const selectContainer = document.getElementById("user-options")
@@ -11,16 +12,22 @@ function EditProfile() {
             update:selected,
             value:newValue
         }
-        // fetch(`http://localhost:9292/user/${id}`,{
-        //   method:"PUT",
-        //   headers:{
-        //     "Content-type":"application/json"
-        //   },
-        //   body:JSON.stringify(changingDetails)
-        // })
-        // .then(response => response.json())
-        // .then(data => console.log(data))
-        // .catch(error => console.log(error))
+        fetch(`http://localhost:9292/user/${id}`,{
+          method:"PUT",
+          headers:{
+            "Content-type":"application/json"
+          },
+          body:JSON.stringify(changingDetails)
+        })
+        .then(response => response.json())
+        .then(data =>{
+          console.log(data)
+          const ul = document.getElementById("success-message")
+          const li = document.createElement('li')
+          li.textContent = "details updated successfully"
+          ul.appendChild(li)
+        })
+        .catch(error => console.log(error))
         console.log(changingDetails)
         setNewvalue("")
     }
@@ -30,6 +37,7 @@ function EditProfile() {
   return (
     <div className='edit-profile'>
         <h2>Select an Option:</h2>
+        <ul id='success-message'></ul>
   <form onSubmit={handleSubmit}>
     <label htmlFor="user-options">Choose the part of your profile you want changed:</label>
     <select id="user-options" name="options">
